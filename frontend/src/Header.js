8c84927e8ccdbe892fd764025254462e2ec658bf
MOHAMMAD KAZIM <65447039+Mohammadkazim07@users.noreply.github.com>
2026-01-03 20:46:59 +0530

    Update Header.js

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import logo from "./assets/logo.png";
-import headerBg from "./assets/banner.jpg"; // 👈 ADD THIS
+import headerBg from "./assets/banner.PNG"; // 👈 ADD THIS
 
 const Header = () => {
   const navigate = useNavigate();
